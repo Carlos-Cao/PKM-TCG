3dae53edbdadae54c89a41d363011bf756941781
carlos-cao <carlos.cao2@gmail.com>
2020-08-13 00:07:12 +1200

    Added search bar

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 function getCards() {
-    fetch("https://api.pokemontcg.io/v1/cards?name=pikachu")
+    const result = document.getElementById("search").value;
+    fetch(`https://api.pokemontcg.io/v1/cards?name=${result}`)
         .then((response) => response.json())
         .then((data) => {
             data.cards.forEach((card) => {
-                console.log(card);
                 document.getElementById("cards").innerHTML +=
-                    ("<img src =" + card.imageUrl + "/>"
+                    ("<img src =" + card.imageUrl + " title='Set: " + card.set + " ' />"
                     )
             });
         });
+}
+
+function removeCards() {
+    document.getElementById("cards").innerHTML = "";
 }
